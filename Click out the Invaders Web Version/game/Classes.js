80ae8cccc7f5db828ab0223f8519eac609f2c9c4
Adee <test@test>
2021-08-09 00:14:34 +0300

    5

---
+++ Click out the Invaders Web Version/game/Classes.js
@@ -0,0 +1,22 @@
+class Worker{
+    constructor(name, number){
+        this.name=name;
+        this.number=number;
+    }
+}
+
+var refugee= new Worker("refugee", 0);
+var peasant= new Worker("peasant", 0);
+var hunter= new Worker("hunter", 0);
+var lumberjack= new Worker("lumberjack", 0);
+var miner= new Worker("miner", 0);
+var mason= new Worker("mason", 0);
+var blacksmith= new Worker("blacksmith", 0);
+var goldsmith= new Worker("goldsmith", 0);
+var armourer= new Worker("armourer", 0);
+var horse= new Worker("horse", 0);
+var merchant= new Worker("merchant", 0);
+
+
+var workerList=[refugee,peasant,hunter,lumberjack,miner,mason,blacksmith,goldsmith,armourer,horse,merchant];
+
--- Click out the Invaders Web Version/game/Economy.js
+++ Click out the Invaders Web Version/game/Economy.js
@@ -2,6 +2,7 @@ var app=angular.module("myApp",[]);
 app.controller("myCtrl",function($scope){
     $scope.score=0;
     $scope.clickedButton='';
+    $scope.workers=['refugeeButton','peasantButton']
 
     $scope.reduceCoin=function(cine){
         var button=document.getElementById(cine);
@@ -22,6 +23,15 @@ app.controller("myCtrl",function($scope){
         console.log("S-a redus");
     }
 
+    $scope.reduceButton=function(cine){
+        var button=document.getElementById(cine);
+        button.style.width="3.4vmin";
+        button.style.height="3.4vmin";
+        button.style.backgroundSize="3.4vmin 3.4vmin";
+        $scope.clickedButton=cine;
+        console.log("S-a redus");
+    }
+
     $scope.resizeButton=function(){
         var button=document.getElementById($scope.clickedButton);
         console.log($scope.clickedButton);
@@ -37,10 +47,27 @@ app.controller("myCtrl",function($scope){
             button.style.fontSize="1.7vw";
             console.log("S-a refacut");
         }
+
+        for(i=0;i<$scope.workers.length;i++){
+            if($scope.clickedButton==$scope.workers[i].name){
+                button.style.width="3.7vmin";
+                button.style.height="3.7vmin";
+                button.style.backgroundSize="3.7vmin 3.7vmin";
+                return;
+            }
+
+        }
     };
 
     $scope.addGold=function(){
         $scope.score+=1;
+    }
+
+    $scope.workers=workerList;
+
+    $scope.buyWorker=function(x){
+        x.number+=1;
+        console.log(workerList[0].number)
     }
 
 });
